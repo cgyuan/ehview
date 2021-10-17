bf799e8799a8a40de7d1cdc45675f341c2f6177e
cgyuan <2515380940@qq.com>
2021-10-17 22:07:29 +0800

    gallery models

---
+++ lib/pages/home/controller/tabpager_controller.dart
@@ -0,0 +1,5 @@
+import 'package:ehviewer/model/gallery_item.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class TabPagerController extends GetxController
+    with StateMixin<List<GalleryItem>> {}
